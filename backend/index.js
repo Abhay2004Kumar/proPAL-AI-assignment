973cac248e75873cad7085c2e2cee19bc7f478f1
Abhay2004Kumar <abs220502@gmail.com>
2025-06-09 00:51:41 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173'], // or the deployed frontend URL
+    origin: ['https://pro-pal-ai-assignment.vercel.app'], // or the deployed frontend URL
     credentials: true,
   }));
 app.use(bodyParser.json());
